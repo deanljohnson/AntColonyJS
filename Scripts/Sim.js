644915065f1b735dd1a1d8c645ebbef9ee75ad29
Dean Johnson <dean@deanljohnson.com>
2015-06-14 05:30:51 +0000

    Fixed bug in FirstSteps tag of Sim.js

--- Scripts/Sim.js
+++ Scripts/Sim.js
@@ -3,13 +3,13 @@ var canvas = document.getElementById("antColonyCanvas");
 function update() {
     //Do updating of simulation here
     
-    //Call update 10 milliseconds after this point
-    setTimeout(update, 10);
+    //Call update at 60 fps
+    setTimeout(update, (1000 / 60));
 }
 
 function draw() {
     //Draw at monitor refresh rate
-    requestAnimationFrame(draw, canvas);
+    requestAnimationFrame(draw);
 }
 
 update();
